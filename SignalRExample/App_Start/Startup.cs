97c8583531dd6343f0061591fb461854eb3e20b5
Semih Ünal <semih1837@gmail.com>
2019-10-31 01:21:48 +0300

    first commit

---
+++ SignalRExample/App_Start/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRExample.App_Start.Startup))]
+
+namespace SignalRExample.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // Uygulamanızı nasıl yapılandıracağınız hakkında daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=316888 adresini ziyaret edin
+        }
+    }
+}
---
+++ SignalRExample/Hubs/ChatHub.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRExample.Models;
+
+namespace SignalRExample.Hubs
+{
+
+    public class ChatHub : Hub
+    {
+
+        static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
+
+        public void SendMessage(string name, string message)
+        {
+            Clients.Others.GetMessageOther(name, message);
+
+            Clients.Caller.GetMessageCaller(message);
+        }
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
+
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+
+            }
+
+        }
+
+       
+
+        public override Task OnConnected()
+        {
+            //    Clients.Others.GetMessageOther(Context.User.Identity.Name, "Kullanıcı Katıldı");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
